3a61de2fadb71be2b6cfda7cfdbedb1d5a5178ec
Daniel Tulpa <dtulpa16@yahoo.com>
2023-10-24 11:53:02 -0600

    Added Styles To Homepage

--- app/components/NewsCard.tsx
+++ app/components/NewsCard.tsx
@@ -4,13 +4,19 @@ type NewsCardProps = {
   article: Article;
   count: number;
 };
+const formatDate = (dateStr : string) => {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const date = new Date(dateStr);
+  return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
 export default function NewsCard({ article, count }: NewsCardProps) {
   return (
     <div className="xl:w-1/3 md:w-1/2 w-full flex gap-5 p-2">
-      <div className=" font-bold text-gray-200 text-3xl">{count < 10 && 0}{count}</div>
+      <div className=" font-bold text-gray-200 text-3xl">{count + 1 < 10 && 0}{count + 1}</div>
       <div className="flex flex-col overflow-auto text-black gap-1">
         <a href="">{article.sourceName}</a>
         <h3 className="line-clamp-2 font-bold">{article.title}</h3>
+        <h4 className="line-clamp-2 font-thin text-sm">{formatDate(article.publishedAt)}</h4>
       </div>
     </div>
   );
--- app/components/NewsFeedList.tsx
+++ app/components/NewsFeedList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Article } from "../lib/types";
 import NewsCard from "./NewsCard";
+import TrendingIcon from "../lib/icons/TrendingIcon";
 export default async function NewsFeedList() {
   const data = await fetch(`${process.env.BASE_URL}/api`);
   let newsList = await data.json();
@@ -9,7 +10,12 @@ export default async function NewsFeedList() {
   ));
   return (
     <div className="flex flex-col pt-11 pb-2">
-      <h2 className="text-black font-semibold text-lg">Trending on Shmedium</h2>
+      <div className="ml-3 flex flex-row gap-4">
+        <TrendingIcon />
+        <h2 className="text-black font-semibold text-lg">
+          Trending on Shmedium
+        </h2>
+      </div>
       <div className="flex flex-wrap">{newsFeed}</div>;
     </div>
   );
