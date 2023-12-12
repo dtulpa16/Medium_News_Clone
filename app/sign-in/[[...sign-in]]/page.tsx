ec85cc329c482fc50cd077424a43c488760a23d1
Daniel Tulpa <dtulpa16@yahoo.com>
2023-12-12 15:48:42 -0700

    Added Margin Top To Sign In & Sign Up Form

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -5,18 +5,20 @@ import { neobrutalism } from "@clerk/themes";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#fbc121]">
+    <>
       <Navbar />
-      <SignIn
-        appearance={{
-          baseTheme: neobrutalism,
-          elements: {
-            formButtonPrimary:
-              "bg-[#ffc017] hover:bg-[#fbc121] text-sm text-black normal-case",
-            footerActionLink: "text-black",
-          },
-        }}
-      />
-    </div>
+      <div className="flex justify-center items-center min-h-screen md:mt-0 mt-6 bg-[#fbc121]">
+        <SignIn
+          appearance={{
+            baseTheme: neobrutalism,
+            elements: {
+              formButtonPrimary:
+                "bg-[#ffc017] hover:bg-[#fbc121] text-sm text-black normal-case",
+              footerActionLink: "text-black",
+            },
+          }}
+        />
+      </div>
+    </>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,21 +1,23 @@
 import Navbar from "@/app/components/Navbar";
 import { SignUp } from "@clerk/nextjs";
 import { neobrutalism } from "@clerk/themes";
- 
+
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#fbc121]">
-      <Navbar/>
-      <SignUp
-        appearance={{
-          baseTheme: neobrutalism,
-          elements: {
-            formButtonPrimary:
-              "bg-[#ffc017] hover:bg-[#fbc121] text-sm text-black normal-case",
-            footerActionLink: "text-black",
-          },
-        }}
-      />
-    </div>
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center min-h-screen md:mt-0 mt-6 bg-[#fbc121]">
+        <SignUp
+          appearance={{
+            baseTheme: neobrutalism,
+            elements: {
+              formButtonPrimary:
+                "bg-[#ffc017] hover:bg-[#fbc121] text-sm text-black normal-case",
+              footerActionLink: "text-black",
+            },
+          }}
+        />
+      </div>
+    </>
   );
 }
