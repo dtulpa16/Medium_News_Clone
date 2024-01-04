44dd2f433b508ddae64430af06866f8401c50c73
Daniel Tulpa <dtulpa16@yahoo.com>
2024-01-04 15:51:57 -0700

    Removed Unused Code + Began Building News Search Functionality

--- app/api/route.ts
+++ app/api/route.ts
@@ -35,24 +35,4 @@ export async function GET(request: Request) {
   }, []);
   data = data.length > 0 ? data : []
   return Response.json({ data })
-  // const headersList = headers()
-  // const { searchParams } = new URL(request.url);
-  // const category = headersList.get("category") || "world";
-  // // const category = searchParams.get("category") || "world";
-  // const page = searchParams.get("page") || "1";
-  // const limit = searchParams.get("limit") || "20";
-
-  // debugger
-  // const res = await fetch(
-  //   `https://newsi-api.p.rapidapi.com/api/category?category=${category}&language=en&country=us&sort=top&page=${page}&limit=${limit}`,
-  //   {
-  //     headers: {
-  //       "X-RapidAPI-Key": process.env.NEWS_API_KEY || "",
-  //       "X-RapidAPI-Host": "newsi-api.p.rapidapi.com",
-  //     }
-  //   },
-
-  // );
-  // const data: Article[] = await res.json();
-  // return Response.json({ data });
 }
--- app/components/SubPageNavBar.tsx
+++ app/components/SubPageNavBar.tsx
@@ -13,17 +13,25 @@ export default function SubPageNavBar() {
             <div className="min-h-[19px] w-[4px] bg-black rounded-e-full rounded-s-full rounded-squeeze-bottom"></div>
           </div>
           {/* Brand  */}
-          <Link href="/" className=" text-[25px] font-bold pl-[3px] hover:cursor-pointer text-black md:mr-4 font-serif tracking-tighter">
+          <Link
+            href="/"
+            className=" text-[25px] font-bold pl-[3px] hover:cursor-pointer text-black md:mr-4 font-serif tracking-tighter"
+          >
             Medium
           </Link>
           {/* Search Bar  */}
           <div className="md:flex items-center bg-[#F9F9F9] rounded-full pl-3 pr-5 min-h-[40px] hidden">
             <SearchIcon className="h-6 w-6 text-gray-500 mr-2" />
-            <input
-              className="bg-[#F9F9F9] outline-none text-sm placeholder-gray-500 placeholder:font-thin placeholder:text-md"
-              type="text"
-              placeholder="Search"
-            />
+            <form action={async(data:FormData)=>{
+                let searchQuery : string = data.get("search")?.toString() || "general"
+            }}>
+              <input
+                className="bg-[#F9F9F9] outline-none text-sm placeholder-gray-500 placeholder:font-thin placeholder:text-md"
+                type="text"
+                name="search"
+                placeholder="Search"
+              />
+            </form>
           </div>
         </div>
         {/* Right tabs  */}
@@ -34,11 +42,16 @@ export default function SubPageNavBar() {
             <p className="text-sm md:block hidden">Write</p>
           </div>
           {/* Sign Up  */}
-          <Link href="/sign-up" className="bg-[#1a8917] text-white text-sm md:px-3 px-2 md:py-2 py-[3px] rounded-full shadow-sm hover:bg-green-600">
+          <Link
+            href="/sign-up"
+            className="bg-[#1a8917] text-white text-sm md:px-3 px-2 md:py-2 py-[3px] rounded-full shadow-sm hover:bg-green-600"
+          >
             Sign up
           </Link>
           {/* Sign In  */}
-          <Link href="/sign-in" className="text-sm">Sign in</Link>
+          <Link href="/sign-in" className="text-sm">
+            Sign in
+          </Link>
           <UserIcon className="h-6 w-6 text-gray-600 md:ml-auto ml-2" />
         </div>
       </div>
