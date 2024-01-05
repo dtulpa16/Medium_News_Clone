66c12f6e3dbe450b50b84b0447596c621a38337e
Daniel Tulpa <dtulpa16@yahoo.com>
2024-01-05 15:48:32 -0700

    Updated SubPageNavbar To Dynamically Render User Icon

--- app/components/SubPageNavBar.tsx
+++ app/components/SubPageNavBar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { UserButton, auth } from "@clerk/nextjs";
 import { SearchIcon, PencilAltIcon, UserIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-export default function SubPageNavBar() {
+export default async function SubPageNavBar() {
+  const { userId }: { userId: string | null } = auth();
   return (
     <nav className="bg-white shadow">
       <div className="w-[97%] mx-auto min-h-[83px] flex justify-between items-center">
-        <div className="content-start flex gap-[1px] items-center align-middle ">
+        <div className="content-start flex gap-[1px] items-center align-middle pl-2">
           {/* Icon  */}
           <div className="flex gap-[1px] items-center">
             <div className="min-h-[23px] w-[23px] bg-black rounded-full"></div>
@@ -16,7 +18,7 @@ export default function SubPageNavBar() {
           {/* Brand  */}
           <Link
             href="/"
-            className=" text-[25px] font-bold pl-[3px] hover:cursor-pointer text-black md:mr-4 font-serif tracking-tighter"
+            className=" text-[25px] font-bold pl-1 hover:cursor-pointer text-black md:mr-4 font-serif tracking-tighter"
           >
             Medium
           </Link>
@@ -48,17 +50,24 @@ export default function SubPageNavBar() {
             <p className="text-sm md:block hidden">Write</p>
           </div>
           {/* Sign Up  */}
-          <Link
-            href="/sign-up"
-            className="bg-[#1a8917] text-white text-sm md:px-3 px-2 md:py-2 py-[3px] rounded-full shadow-sm hover:bg-green-600"
-          >
-            Sign up
-          </Link>
-          {/* Sign In  */}
-          <Link href="/sign-in" className="text-sm">
-            Sign in
-          </Link>
-          <UserIcon className="h-6 w-6 text-gray-600 md:ml-auto ml-2" />
+          {!userId ? (
+            <>
+              <Link
+                href="/sign-up"
+                className="bg-[#1a8917] text-white text-sm md:px-3 px-2 md:py-2 py-[3px] rounded-full shadow-sm hover:bg-green-600"
+              >
+                Sign up
+              </Link>
+              {/* Sign In  */}
+              <Link href="/sign-in" className="text-sm">
+                Sign in
+              </Link>
+            </>
+          ) : (
+            <div className="mr-2">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          )}
         </div>
       </div>
     </nav>
