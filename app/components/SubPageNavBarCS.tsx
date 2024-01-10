736b8b8c2975739d6f44dab7bb0c54175fa9ff28
Daniel Tulpa <86633589+dtulpa16@users.noreply.github.com>
2024-01-09 18:17:53 -0700

    Import Mod

--- app/components/SubPageNavBarCS.tsx
+++ app/components/SubPageNavBarCS.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React,{useState} from "react";
 import { UserButton } from "@clerk/nextjs";
 import { useAuth } from "@clerk/nextjs";
 import { SearchIcon, PencilAltIcon } from "@heroicons/react/outline";
